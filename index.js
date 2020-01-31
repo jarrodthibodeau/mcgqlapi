615c35466d4728ac0e1074116d209508de0015d7
Jarrod Thibodeau <thibodeaujz@gmail.com>
2020-01-31 11:53:43 -0500

    moving server into its own file (#17)
    * moving server into its own file
    
    * fixing broken test

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const path = require('path');
-const root = require('./src/root');
-const schema = require('./src/schema');
 const logger = require('./src/helpers/logger');
-const { ApolloServer } = require('apollo-server-lambda');
-
-const server = new ApolloServer({ 
-  typeDefs: schema, 
-  resolvers: root, 
-  playground: process.env.ENVIRONMENT === 'prod' ? false : true,
-});
+const server = require('./src/server');
 
 require('dotenv').config({
     path: path.resolve(__dirname, `./${process.env.ENVIRONMENT}.env`)
--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
-const server = require('../index').server;
+const server = require('../src/server');
 const testServer = require('apollo-server-testing');
 
 process.env.SAVE_TO_DB = false;
+process.env.DAYS_TIL_SAVE_TO_DB = 30;
 
 module.exports = testServer.createTestClient(server);
