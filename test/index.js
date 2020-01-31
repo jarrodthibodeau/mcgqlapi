e2df13d18077cab310d54343b2574a3b67fdd3e6
Jarrod Thibodeau <thibodeaujz@gmail.com>
2020-01-31 11:52:30 -0500

    fixing broken test

--- test/index.js
+++ test/index.js
@@ -2,5 +2,6 @@ const server = require('../src/server');
 const testServer = require('apollo-server-testing');
 
 process.env.SAVE_TO_DB = false;
+process.env.DAYS_TIL_SAVE_TO_DB = 30;
 
 module.exports = testServer.createTestClient(server);
