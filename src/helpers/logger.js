7dfee12c8e7aa021cdd2fa61522217e668e50f8a
Jarrod Thibodeau <thibodeaujz@gmail.com>
2020-02-03 20:55:33 -0500

    adding scenario for half seasons (#18)
    * adding scenario for half seasons
    
    * fix formatting

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -5,7 +5,7 @@ const logger = winston.createLogger({
     transports: [
         new winston.transports.Console(),
         new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'combined.log '})
+        new winston.transports.File({ filename: 'combined.log'})
     ]
 });
 
