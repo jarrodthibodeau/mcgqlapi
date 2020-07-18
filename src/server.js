b2f2e4dc5dff31d9776cff294c3a4b5ceff21c14
Jarrod Thibodeau <thibodeaujz@gmail.com>
2020-07-18 17:00:59 -0400

    setting up dev-server capabilities and adding much needed docs to the schema (#21)

--- src/server.js
+++ src/server.js
@@ -1,11 +1,25 @@
 const root = require('./root');
 const schema = require('./schema');
 const { ApolloServer } = require('apollo-server-lambda');
+const devServer = require('apollo-server');
 
-const server = new ApolloServer({ 
-  typeDefs: schema, 
-  resolvers: root, 
-  playground: process.env.ENVIRONMENT === 'prod' ? false : true,
-});
+let server;
+
+if (process.env.ENVIRONMENT === 'prod') {
+  server = new ApolloServer({ 
+    typeDefs: schema, 
+    resolvers: root, 
+    playground: true,
+  });
+} else {
+  server = new devServer.ApolloServer({
+    typeDefs: schema, 
+    resolvers: root, 
+    playground: true,
+  });
+}
+
+
+
 
 module.exports = server;
